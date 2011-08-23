9edee57b6044a0b9b9bb5efc07703db95f3912aa
Andrew Marrone <github@andrewmarrone.com>
2011-08-23 16:40:36 -0700

    getting time for problem 1

--- 1.py
+++ 1.py
@@ -1,3 +1,6 @@
+import core
+
+start = core.time()
 mult3 = range (0, 1000, 3)
 mult5 = range (0, 1000, 5)
 
@@ -7,5 +10,6 @@ result = 0
 
 for i in set(dif) :
 	result += i
-
+    
 print result
+print "Elapsed time: " + str(core.time() - start) + " seconds"
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 Solved Problems Runtime
 =========================
 
+1.  Elapsed time: 0.000356912612915 seconds
 10. Elapsed time: 25.3259310722 seconds
