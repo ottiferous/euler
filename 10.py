4e697b78aee0c7396b3c9738ed0e7d416afe6739
Andrew Marrone <github@andrewmarrone.com>
2011-08-23 15:43:05 -0700

    changed to work with core library

--- 10.py
+++ 10.py
@@ -1,26 +1,9 @@
-def isprime(n):
-	n = abs(int(n))
-	if n < 2:
-		return False
-	if n == 2:
-		return True
-	if not n & 1:
-		return False
-	for x in range(3, int(n**0.5)+1, 2):
-		if n % x == 0:
-			return False
-	return True
-
-def primelist(n):
-	a = []
-	for i in range(2,n):
-		if isprime(i):
-			a.append(i)
-	return a
+import core
+start = core.time()
 
 ubound = input ('Find primes up to: ')
 
-list = primelist(ubound)
+list = core.primelist(range(0, ubound))
 
 hold = 0
 
@@ -28,3 +11,4 @@ for _ in list:
 	hold += _	
 
 print "Summation is: " + str(hold)
+print "Elapsed time: " + str(core.time() - start) + " seconds"
