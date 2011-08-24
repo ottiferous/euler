aefc412798ea3919a5db99ae80cdfcacc0e0936c
Andrew Marrone <github@andrewmarrone.com>
2011-08-23 17:11:15 -0700

    updated project 2 to use core library

--- 2.py
+++ 2.py
@@ -1,14 +1,11 @@
-import time
-start = time.time()
-
-def F(max):
-    a, b = 0, 1
-    while a < max:
-        yield a
-        a, b = b, a+b
+import core
+start = core.time()
 
 result = 0
-for n in F(4000000):
-   if n % 2 == 0: result += n
+n = 0
+f = core.fib()
+while n < 4000001:
+    n = f.next()
+    if n % 2 == 0: result += n
 print result
-print "Completed in " + str(time.time() - start) + " seconds"
+print "Completed in " + str(core.time() - start) + " seconds"
