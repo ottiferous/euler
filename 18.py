cd9c3c9d380144d1817d35688a978214d3ba025b
Andrew Marrone <github@andrewmarrone.com>
2011-08-27 18:28:19 -0700

    New direction with problem 18

--- 18.py
+++ 18.py
@@ -1,35 +1,50 @@
-import time
+#
+#   Screw summing it and using maths. Time for logic
+#
 
-#   Gets pyramid from file and retuns list of each row
-def getpyramid(name):
+#   Test Variables for loading into python repl
 
-    biglist = [] 
-    f = open(name, 'r')
-    for line in f.read().split('\n'):
-        biglist.append(line)
-    f.close()
-    del biglist[-1]
+list = [[10], [11, 12], [13, 14, 15], [16, 17, 18, 19]]
 
-    return biglist
-
-#   Takes an array of characters to sum
-#       returns a list of ints for that row
-def sum(row):
+#   Sum the numbers in pairs
+def sumrow(row):
     
-    result = []
-    for _ in xrange(len(row)-1):
-        result.append((int(row[_])+int(row[_+1])))
+    if(len(row) == 1): return row
 
+    result = []
+    for x in range(len(row)-1):
+        result.append(row[x] + row[x+1])
     return result
 
-#   sum values of row returning values to be
-#       added to the row above in a 1:1 ratio
-def makepaths(tri):
+#   Gets pyramid from file and retuns list of each row
+#       as a literal string. Returns pyramid upside-down
+def getpyramid():
 
-#   Need to start at the 'bottom' and work our way up
-   tri.reverse()
-   new = []
+    biglist = []
+    f = open('18list', 'r')
 
-   for row in tri:
-       new.append(sum(row))
+    for line in f.read().split('\n'):
+        b = []
+        for x in line:
+            b.append(int(x))
+        biglist.append(b)
 
+    f.close()
+    del biglist[-1]
+    biglist.reverse()
+
+    return biglist    #   returned array is 'upside-down'
+
+#   Walk through the pyramid
+#       takes the completed array
+#       and finds the total score
+def score(list):
+   
+    list.reverse()
+
+    total = 0
+    for row in list[:1]:
+        total += max(sumrow(row))
+    total += max(list[0])
+
+    return total
