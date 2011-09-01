87cd6b54e45ab2d32f694cecc0bc3be9569a14bc
Andrew Marrone <github@andrewmarrone.com>
2011-09-01 14:29:40 -0700

    working on project 22

---
+++ 22.py
@@ -0,0 +1,30 @@
+from time import time
+import csv
+
+# Get the names into an array for sorting
+def getnames():
+
+    a = []
+    names = csv.reader(open('22list.txt', 'rb'), delimiter = ',', quotechar='"')
+    for row in names.next():
+        a.append(row)
+    return a
+
+#   Score the name based on its characters
+def scoreof(name):
+
+    score = 0
+    for i in name:
+        score = score + ord(i) - 64
+    return score
+
+#   Solve the Problem
+def proj22():
+    
+    list = getnames()
+    list.sort()
+    points = 0
+
+    for item in range(len(list)):
+        points += (scoreof(list[item]) * item)
+    return points
