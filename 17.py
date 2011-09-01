8b6e20f096139f640841fb185b87afb7ea4b47d8
Andrew Marrone <github@andrewmarrone.com>
2011-08-31 20:40:35 -0700

    Start to work on project 17

---
+++ 17.py
@@ -0,0 +1,71 @@
+#   If the numbers 1 to 5 are written out in words: one, two, three, four, five, 
+#   then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+#
+#   If all the numbers from 1 to 1000 (one thousand) inclusive 
+#   were written out in words, how many letters would be used?
+
+numdict = {
+    0   :   "",
+    1   :   "one",
+    2   :   "two",
+    3   :   "three",
+    4   :   "four",
+    5   :   "five",
+    6   :   "six",
+    7   :   "seven",
+    8   :   "eight",
+    9   :   "nine",
+    10  :   "ten",
+    11  :   "eleven",
+    12  :   "twelve",
+    13  :   "thirteen",
+    14  :   "fourteen",
+    15  :   "fifteen",
+    16  :   "sixteen",
+    17  :   "seventeen",
+    18  :   "eighteen",
+    19  :   "nineteen",
+    20  :   "twenty",
+    30  :   "thirty",
+    40  :   "fourty",
+    50  :   "fifty",
+    60  :   "sixty",
+    70  :   "seventy",
+    80  :   "eighty",
+    90  :   "ninety",
+    100 :   "hundred",
+    1000:   "thousand"
+}
+
+#   find the letters of a given number
+def lettersof(number):
+    
+    if number < 21:
+        return len(numdict[number])
+    
+    i = word = 0
+    
+
+    i = int(number/100)
+    if i > 0:
+        word += len(numdict[i]) + len(numdict[100])
+
+    number -= i*100
+    
+    i = int(number/10)
+    if i > 0:
+        word += len(numdict[i*10])
+    number -= i*10
+
+    word += len(numdict[number])
+
+    return word
+        
+
+#   Now begins the actual code
+def proj17():
+
+    count = 0
+    for num in range(1, 10001):
+        count += lettersof(num)
+    return count
