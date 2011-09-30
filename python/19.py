1267b1a935a667dc05a45af26971244096d68c3f
Andrew Marrone <github@andrewmarrone.com>
2011-09-30 14:54:57 -0700

    finished 19

---
+++ python/19.py
@@ -0,0 +1,17 @@
+import calendar
+
+year = 1901
+month = 01
+
+count = 0
+
+for year in xrange(1901, 2001):
+
+    for month in xrange(1, 13):
+
+        if calendar.weekday(year,month,1) == 1:
+            count += 1
+
+print count
+
+
