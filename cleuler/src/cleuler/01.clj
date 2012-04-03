fa1557f3b69b7fe8c4b22687bdc2fa8096102d80
Andrew Marrone <github@andrewmarrone.com>
2012-04-02 21:10:48 -0700

    removed closure

--- cleuler/src/cleuler/01.clj
+++
@@ -1,6 +0,0 @@
-(ns cleuler.01
-	(:use [cleuler.core]))
-
-(reduce + (filter #(or (zero? (mod % 3))(zero? (mod % 5))) (range 1 1000)))
-
-(println "Haha dogs look funny in hats")
--- cleuler/src/cleuler/core.clj
+++
@@ -1,6 +0,0 @@
-(ns cleuler.core)
-
-(defn square [x]
-	(* x x))
-(defn times [x]
-	(* 3 x))
