b24415db0779a7f46038a8857497e07e1f5ce940
Andrew <github@andrewmarrone.com>
2015-05-08 11:13:27 -0700

    Update 06.py
    simplified loop and added final calculation for difference

--- python/06.py
+++ python/06.py
@@ -1,11 +1,10 @@
 sumofsquare = 0
 squareofsum = 0
 
-count = 1
 
-for _ in range(100):
+for count in range(100):
 	sumofsquare += (count**2)
 	squareofsum += count
-	count += 1
 	
 squareofsum = squareofsum**2
+print str(squareofsum - sumofsquare)
