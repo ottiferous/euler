36b4f7bfdd85a84bde766fa920f470a4ac6c9bf8
Andrew <github@andrewmarrone.com>
2015-05-08 15:30:32 -0700

    removed multiple conditional breaks
    changed into a function to make the exit process easier. product is calculated with reduce() and anonymous function

--- python/09.py
+++ python/09.py
@@ -1,12 +1,10 @@
-a, b, c = 3,4,5
-for a in range(3,500):
-	for b in range(a+1,500):
-		for c in range(b+1,500):
-			if (c*c) == (b*b + a*a):
+a, b, c = 3, 4, 5
 
-				if (a+b+c) == 1000:
-					break
-		if a+b+c == 1000:
-			break
-	if a+b+c == 1000:
-		break
+def pythTripletGenerator():
+    for a in xrange(3, 500):
+        for b in xrange(a+1, 500):
+            for c in xrange(b+1, 500):
+                if ((c**2 == b**2 + a**2) and (a+b+c == 1000)):
+                    return (a, b, c)
+
+print reduce(lambda x,y: x*y, pythTripletGenerator())
