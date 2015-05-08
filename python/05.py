d67835b0453c472fb971c241d37ca90b0361436a
Andrew <github@andrewmarrone.com>
2015-05-08 11:12:18 -0700

    Update 05.py
    cleaned up long conditional in loop

--- python/05.py
+++ python/05.py
@@ -1,26 +1,9 @@
 seed = 19*17*13*11
-test = reduce(int.__mul__, xrange(1, 21), 1)
+test = reduce(lambda x, y: x*y, xrange(2, 20)
 count = 1
 
 while seed < test:
-	if ((seed*count) % 20 == 0) \
-		and ((seed*count) % 19 == 0) \
-		and ((seed*count) % 18 == 0) \
-		and ((seed*count) % 17 == 0) \
-		and ((seed*count) % 16 == 0) \
-		and ((seed*count) % 15 == 0) \
-		and ((seed*count) % 14 == 0) \
-		and ((seed*count) % 13 == 0) \
-		and ((seed*count) % 12 == 0) \
-		and ((seed*count) % 11 == 0) \
-		and ((seed*count) % 10 == 0) \
-		and ((seed*count) %  9 == 0) \
-		and ((seed*count) %  8 == 0) \
-		and ((seed*count) %  7 == 0) \
-		and ((seed*count) %  6 == 0) \
-		and ((seed*count) %  4 == 0) \
-		and ((seed*count) %  3 == 0) \
-		and ((seed*count) %  2 == 0): 
-			print "Result is: " + str(seed*count)
-			break
+	if all(seed*count % n == 0 for n in xrange(2, 20)):
+		print "Result is: " + str(seed*count)
+		break
 	count += 1
